ad59728321885c7febc885e1c53f86c769a589a2
Garinyr <garinyr92@gmail.com>
2022-06-27 15:29:57 +0700

    fix

--- Test Suites/test.ts
+++ Test Suites/test.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>NOW 417</name>
+   <name>test</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -16,59 +16,69 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC001-No Kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b278f54a-82bd-4665-ace7-ceb72292c6f7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC002-Salah No HP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>d3790277-6358-4435-ada3-d73e24646b4b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC003-Min No HP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>36f03f71-34eb-4adc-838d-20a22f53db4c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC004-salah input OTP</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>61ae51c8-64f1-4469-8431-4e3356613df6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC005-Nama lengkap Kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>513d7635-1c0d-440b-9985-49e091c934fc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC006-ISI dengan special karakter dan number</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>744609f6-7b20-4709-a8b4-640cbfb26d04</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC007-Pin Tidak sama</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>935f5bd0-d56c-4c95-833b-bb10dde39bba</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC008-Password Kosong dan Konfirmasi password Kosong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>f7511968-e652-4602-b8b0-2c47433f2ec0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC009-Password dan Konfirmasi password Tidak Sama</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>65e07502-1710-4459-a431-dc9c7d9ce667</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Lestari/Create Test Script Automation Android/Now 366/First New Downloader/Now 417/TC010- Registrasi Berhasil</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
